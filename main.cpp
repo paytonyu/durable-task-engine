219287f5a5463c7ed434a9544e2f9585fab69404
payton <payton.yu0620@gmail.com>
2026-08-05 21:26:38 -0700

    added BoundedQueue class skeleton with capacity constructor

--- main.cpp
+++ main.cpp
@@ -1,5 +1,36 @@
 #include <iostream>
 #include <thread>
+#include <queue>
+#include <mutex>
+#include <condition_variable>
+
+template <typename T>
+
+class BoundedQueue {
+    private:
+        std::condition_variable not_full;
+        std::condition_variable not_empty;
+        std::mutex mtx;
+        std::queue<T> container;
+        const size_t capacity;
+
+
+    public:
+        BoundedQueue(size_t x): capacity{x};
+        void push(T){
+
+        }
+        std::optional<T> pop(){
+
+        }
+        void shutdown(){
+
+        }
+        size_t size() const{
+
+        }
+};
+
 
 int main() {
     std::cout << "Initializing Durable Task Engine (C++17)..." << std::endl;
