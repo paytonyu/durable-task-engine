ad943cb426949245b848198d208db80b62486b81
payton <payton.yu0620@gmail.com>
2026-08-08 16:01:26 -0700

    Push implemented and tested

--- main.cpp
+++ main.cpp
@@ -16,10 +16,18 @@ class BoundedQueue {
 
 
     public:
-        BoundedQueue(size_t x): capacity{x};
-        void push(T){
+        BoundedQueue(size_t x): capacity{x}{}
+        void push(const T& insert){
+            std::unique_lock<std::mutex> lock(mtx);
 
+            not_full.wait(lock, [this]{ return container.size() < capacity;});    
+            
+            container.push(insert);
+
+            not_empty.notify_one();
         }
+
+
         std::optional<T> pop(){
 
         }
@@ -29,16 +37,35 @@ class BoundedQueue {
         size_t size() const{
 
         }
+        
 };
 
 
 int main() {
-    std::cout << "Initializing Durable Task Engine (C++17)..." << std::endl;
-    
-    std::thread t([]() {
-        std::cout << "Worker thread initialization check: SUCCESS" << std::endl;
-    });
-    t.join();
+    BoundedQueue<int> queue(3);
 
+    std::cout << "Initializing Durable Task Engine (C++17)..." << std::endl;
+    bool thread_finished = false;
+
+    std::thread t([&thread_finished, &queue]() {
+        queue.push(123);
+        queue.push(123);
+        queue.push(123);
+    });
+
+    std::thread t2([&thread_finished, &queue]() {
+        queue.push(123);
+        thread_finished = true;
+    });
+
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+
+    if (thread_finished == true) {
+        std::cout << "push works" << std::endl;
+    } else {
+        std::cout << "push blocked" << std::endl;
+    }
+
+    t.join();
     return 0;
 }
