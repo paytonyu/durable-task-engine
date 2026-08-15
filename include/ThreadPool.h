543c0a725e8bd6e3ad07ab20e3d673ed37c873c0
payton <payton.yu0620@gmail.com>
2026-08-14 23:13:06 -0700

    fixes to threadpool, constructor complete

--- include/ThreadPool.h
+++ include/ThreadPool.h
@@ -1,18 +1,23 @@
 #include "BoundedQueue.h"
+#include <functional>
+#cinlude <vector>
 
 template <typename T>
 
 class ThreadPool{
     private:
+        BoundedQueue<std::fucntion<void()>> taskQueue; // taskqueue must be written above workers to show dependacy of workers on taskqueue
         std::vector<std::thread> workers;
 
+        void workerLoop(){
+            
+        }
     
     public:
+        ThreadPool(size_t x): taskQueue{x}{}
         ThreadPool(size_t workerCount) {
-            for (size_t i = 0; i < workers; i ++){
-
-
-
+            for (size_t i = 0; i < workerCount; i ++){
+                workers.emplace_back(&ThreadPool::workerLoop, this);
             }
         }
 };
