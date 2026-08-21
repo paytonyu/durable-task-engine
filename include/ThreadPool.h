7a9af5c4775e1fce82894f001998786c8e702e97
payton <payton.yu0620@gmail.com>
2026-08-20 18:47:44 -0700

    threadpool with worker look and shutdown added

--- include/ThreadPool.h
+++ include/ThreadPool.h
@@ -1,23 +1,40 @@
 #include "BoundedQueue.h"
 #include <functional>
-#cinlude <vector>
-
-template <typename T>
+#include <vector>
+#include <iostream>
+#include <thread>
 
 class ThreadPool{
     private:
-        BoundedQueue<std::fucntion<void()>> taskQueue; // taskqueue must be written above workers to show dependacy of workers on taskqueue
+        BoundedQueue<std::function<void()>> taskQueue; // taskqueue must be written above workers to show dependacy of workers on taskqueue
         std::vector<std::thread> workers;
-
         void workerLoop(){
-            
+            while (true) {
+                auto result = taskQueue.pop();
+                if (!result) break;
+                try {
+                    result.value()(); 
+                } catch (const std::exception& e) {
+                    std::cerr << "task threw: " << e.what() << std::endl; // standard catch describing what went wrong through e.what()
+                } catch (...) { //catch anything that passes first catch
+                    std::cerr << "task threw unknown exception" << std::endl;
+                }
+            }
         }
     
     public:
-        ThreadPool(size_t x): taskQueue{x}{}
-        ThreadPool(size_t workerCount) {
+        ThreadPool(size_t workerCount) : taskQueue(1024){
             for (size_t i = 0; i < workerCount; i ++){
                 workers.emplace_back(&ThreadPool::workerLoop, this);
             }
         }
+        ~ThreadPool() {
+            taskQueue.shutdown();
+            for (std::thread& w : workers) {
+                if (w.joinable()) {
+                    w.join();
+                }
+            }
+        }
+
 };
